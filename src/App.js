e87ce5c1a3efd3eeb693442fd72d6887fb8596d4
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-17 16:42:45 -0700

    fix linting errors to deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import PokemonSearch from './PokemonSearch';
-import YelpSearch from './YelpSearch';
-import WeatherSearch from './WeatherSearch';
+// import PokemonSearch from './PokemonSearch';
+// import YelpSearch from './YelpSearch';
+// import WeatherSearch from './WeatherSearch';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <PokemonSearch />
-      <YelpSearch />
-    { /* stretch goal: <WeatherSearch /> */ }
+      {/* <PokemonSearch />
+      <YelpSearch /> */}
+      {/* stretch goal: <WeatherSearch /> */}
     </div>
   );
 }
